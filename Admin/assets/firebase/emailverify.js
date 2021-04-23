5bad5d1b592d7f1f8db833ea95bd4239776d0f96
Adish Pathare <adish.pathare@gmail.com>
2021-04-23 21:40:31 +0530

    Update emailverify.js

--- Admin/assets/firebase/emailverify.js
+++ Admin/assets/firebase/emailverify.js
@@ -1,19 +1,20 @@
+
+//Check whether user email address is verified or not
 firebase.auth().onAuthStateChanged(function(user) {
     console.log(user)
     if (user.emailVerified) {
         console.log('Email is verified');
         console.log(user);
     } else {
-        // window.location = "index.html";
-        // firebase.auth().signOut();
         console.log("Email is not verified");
         $("#verify").show();
     }
 });
 
-function verify(){
 
-    firebase.auth().onAuthStateChanged(function(user) {
+//Send Email Verification Link
+function verify(){
+      firebase.auth().onAuthStateChanged(function(user) {
          if (user) {
             user.sendEmailVerification().then(function(){
                 console.log("email verification sent to user");
