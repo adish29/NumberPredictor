81478c11da8dfa5a02ca639920a0f21595036ab8
Adish Pathare <adish.pathare@gmail.com>
2021-04-23 14:53:32 +0530

    Email verification

---
+++ Admin/assets/firebase/emailverify.js
@@ -0,0 +1,23 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    console.log(user)
+    if (user.emailVerified) {
+        console.log('Email is verified');
+        console.log(user);
+    } else {
+        // window.location = "index.html";
+        // firebase.auth().signOut();
+        console.log("Email is not verified");
+        $("#verify").show();
+    }
+});
+
+function verify(){
+
+    firebase.auth().onAuthStateChanged(function(user) {
+         if (user) {
+            user.sendEmailVerification();
+        }
+    });
+
+
+}
